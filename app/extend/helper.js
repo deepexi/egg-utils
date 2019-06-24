620d2dc5508793ad6f1e661c8db4eb593114addb
qiye <lujunwei@deepexi.com>
2019-06-24 19:27:26 +0800

    SSEUtils绑定到helper对象中

--- app/extend/helper.js
+++ app/extend/helper.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const HttpUtils = require('./utils/http_utils');
+const SSEUtils = require('./utils/sse_utils');
 
 module.exports = {
   get http() {
     return new HttpUtils(this.ctx.logger);
   },
+  SSEUtils: SSEUtils
 };
