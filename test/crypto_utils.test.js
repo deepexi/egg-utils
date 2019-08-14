adee3dfe07e60bf3416961e8de63719dc953aee6
zhangyongbin <ybzhang1212@163.com>
2019-08-14 19:41:19 +0800

    refactor: optimize the description

--- test/crypto_utils.test.js
+++ test/crypto_utils.test.js
@@ -6,12 +6,12 @@ const assert = require('assert');
 
 describe('#crypto_utils.js', () => {
 
-  it('should be return string', function() {
+  it('should be return string without salt value', function() {
     const md5 = DigestUtils.md5('data');
     assert.equal(md5.length, 32);
   });
 
-  it('should be return string', function() {
+  it('should be return string with salt value', function() {
     const md5 = DigestUtils.md5('data', 'dada');
     assert.equal(md5.length, 32);
   });
@@ -22,7 +22,7 @@ describe('#crypto_utils.js', () => {
     }, false);
   });
 
-  it('should be throw error', function() {
+  it('should be throw error salt is must be string', function() {
     assert.throws(() => {
       DigestUtils.md5('data', { salt: 'salt' });
     });
@@ -30,7 +30,7 @@ describe('#crypto_utils.js', () => {
 
 
   // aes加密
-  it('aes should be throw error', function() {
+  it('aes should be throw error secretKey or iv length must be 16', function() {
     assert.throws(() => {
       DigestUtils.aesEncrypt('data', 'abcd', 'abc');
     });
@@ -41,26 +41,26 @@ describe('#crypto_utils.js', () => {
     assert.equal(aes.length, 32);
   });
 
-  it('aes should be throw error', function() {
+  it('aes should be throw error data type is not suitable', function() {
     assert.throws(() => {
       DigestUtils.aesEncrypt({ data: 'data' }, 'abcdabcdabcdabcd', 'abcdabcdabcdabcd');
     });
   });
 
   // aes 解密
-  it('should be return string', function() {
+  it('should be return data', function() {
     const aes = DigestUtils.aesEncrypt('data', 'abcdabcdabcdabcd', 'abcdabcdabcdabcd');
     const result = DigestUtils.aesDecrypt(aes, 'abcdabcdabcdabcd', 'abcdabcdabcdabcd');
     assert.equal(result, 'data');
   });
 
-  it('should be return string', function() {
+  it('should be return null is not suitable', function() {
     const aes = DigestUtils.aesEncrypt('data', 'abcdabcdabcdabcd', 'abcdabcdabcdabcd');
     const result = DigestUtils.aesDecrypt(aes, 'abcdabcdabcdabda', 'abcdabcdabcdabcd');
     assert.equal(result, null);
   });
 
-  it('should be throw error', function() {
+  it('should be throw error dataType is not suitable', function() {
     const aes = DigestUtils.aesEncrypt('data', 'abcdabcdabcdabcd', 'abcdabcdabcdabcd');
     assert.throws(() => {
       DigestUtils.aesDecrypt({ aes }, 'abcdabcdabcdabda', 'abcdabcdabcdabcd');
